e52f953b0c4223e9b1f7f9d16ff00266e286c43f
Beniamin Dobre <bdobre@fitbit.com>
2018-03-01 14:24:09 +0200

    use zxing and BARCODE API

---
+++ app/src/main/java/com/example/android/licenta/bl/BusinessLogic.java
@@ -0,0 +1,83 @@
+package com.example.android.licenta.bl;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Hashtable;
+
+/**
+ * Created by bdobre on 2/22/18.
+ */
+
+public class BusinessLogic {
+
+    public static Bitmap getQrCode(String text, int size) {
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>(2);
+            hints.put(EncodeHintType.CHARACTER_SET, "ISO-8859-1");
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, size, size, hints);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return Bitmap.createBitmap(size, size, Bitmap.Config.ALPHA_8);
+        }
+    }
+
+    public static byte[] readFile(File file) {
+        int size = (int) file.length();
+        byte[] bytes = new byte[size];
+        try {
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes, 0, bytes.length);
+            buf.close();
+            return bytes;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new byte[size];
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return new byte[size];
+        }
+    }
+
+    public static byte[] fullyReadFileToBytes(File f) {
+        int size = (int) f.length();
+        byte bytes[] = new byte[size];
+        byte tmpBuff[] = new byte[size];
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(f);
+            int read = fis.read(bytes, 0, size);
+            if (read < size) {
+                int remain = size - read;
+                while (remain > 0) {
+                    read = fis.read(tmpBuff, 0, remain);
+                    System.arraycopy(tmpBuff, 0, bytes, size - remain, read);
+                    remain -= read;
+                }
+            }
+            fis.close();
+        }  catch (IOException e){
+            Log.i("HELLO", "fullyReadFileToBytes: FAILED");
+        }
+
+        return bytes;
+    }
+}
---
+++ app/src/main/java/com/example/android/licenta/ui/EncodeActivity.java
@@ -0,0 +1,74 @@
+package com.example.android.licenta.ui;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.example.android.licenta.R;
+import com.example.android.licenta.bl.BusinessLogic;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+
+public class EncodeActivity extends AppCompatActivity {
+    private ImageView qrImageView;
+    String text = "/storage/emulated/0/Download/images.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        File imgFile = new  File("/sdcard/Images/test_image.jpg");
+//        File imgFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Orar3CC.xls");
+
+        byte[] rez = {0x1B, 0x2A, 0x7F, -0x80};
+        String str = "BENI";
+        try {
+            str = new String(rez,"ISO-8859-1");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+//        byte[] rez2;
+//        try {
+//            rez2 = str.getBytes("ISO-8859-1");
+//        } catch (UnsupportedEncodingException e) {
+//            Log.i("BENI", "file not present");
+//        }
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        qrImageView = findViewById(R.id.qr_image_view);
+        qrImageView.setImageBitmap(BusinessLogic.getQrCode(str, metrics.widthPixels));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.decode, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.decode_qr:
+                Intent intent = new Intent(this, DecodeActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
